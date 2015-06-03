faf1f80c18e6379fd32693c8e7b6aacf790520b5
Igor Escobar <blog@igorescobar.com>
2015-06-02 22:21:15 -0300

    testing router

--- ToyRobot/spec/router_spec.rb
+++ ToyRobot/spec/router_spec.rb
@@ -1,15 +1,47 @@
 require "./src/router.rb"
 
 describe Router do
-  context "#directions" do
-    it ""
+  context "#next_route" do
+    context "north" do
+      let(:router) { Router.new(1, 1, :north) }
+      subject { router.next_route }
+
+      it { is_expected.to eql [1, 2] }
+    end
+
+    context "east" do
+      let(:router) { Router.new(1, 1, :east) }
+      subject { router.next_route }
+
+      it { is_expected.to eql [2, 1] }
+    end
+
+    context "south" do
+      let(:router) { Router.new(1, 1, :south) }
+      subject { router.next_route }
+
+      it { is_expected.to eql [1, 0] }
+    end
+
+    context "west" do
+      let(:router) { Router.new(1, 1, :west) }
+      subject { router.next_route }
+
+      it { is_expected.to eql [0, 1] }
+    end
   end
 
   context "#turn_left" do
-    it ""
+    let(:router) { Router.new(1, 1, :north) }
+    subject { router.turn_left }
+
+    it { is_expected.to be :west }
   end
 
   context "#turn_right" do
-    it ""
+    let(:router) { Router.new(1, 1, :north) }
+    subject { router.turn_right }
+
+    it { is_expected.to be :east }
   end
 end
